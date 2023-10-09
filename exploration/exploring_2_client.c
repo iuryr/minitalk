11a2534b3da9dfd3de315fc531edc0111181ec3d
iury <iuryroberto@gmail.com>
2023-10-09 10:44:40 -0300

    pra ir pra 42

--- exploration/exploring_2_client.c
+++ exploration/exploring_2_client.c
@@ -1,10 +1,33 @@
+#include <unistd.h>
+#include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
+
+void	sigusr1_handler(int signum, siginfo_t *info, void *)
+{
+	if (signum == SIGUSR1)
+		printf("SIGUSR1 Received from process %d\n", info->si_pid);
+}
 
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
 		return (-1);
 
+	struct sigaction sa;
+	sa.sa_sigaction = sigusr1_handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+
+	printf("Sending first signal...\n");
+	sleep(1);
 	kill(atoi(argv[1]), SIGUSR1);
+	while(1)
+	{
+		printf("Waiting for ack signal..\n");
+		sleep(1);
+		kill(atoi(argv[1]), SIGUSR1);
+		pause();
+	}
 }
--- exploration/exploring_2_server.c
+++ exploration/exploring_2_server.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 #include <signal.h>
 
-void	sigusr1_handler(int signum)
+void	sigusr1_handler(int signum, siginfo_t *info, void *)
 {
 	if (signum == SIGUSR1)
-		printf("SIGUSR1 Received!\n");
+	{
+		printf("SIGUSR1 Received from process %d\n", info->si_pid);
+		kill(info->si_pid, SIGUSR1);
+	}
 }
 
 int main(void)
@@ -16,8 +19,8 @@ int main(void)
 	printf("Server PID: %i\n", pid);
 
 	struct sigaction sa;
-	sa.sa_handler = sigusr1_handler; //indicando nossa funcao que vai lidar com o sinal
-	sa.sa_flags = 0; //avisando que nosso processo deve executar a signal handler de maneira "padrao"
+	sa.sa_sigaction = sigusr1_handler; //indicando nossa funcao que vai lidar com o sinal
+	sa.sa_flags = SA_SIGINFO; //avisando que nosso processo deve executar a signal handler de maneira "padrao"
 	sigemptyset(&sa.sa_mask); //inicializando o sa_mask para vazio, ou seja, nao ha sinais para serem bloqueados.
 	
 	sigaction(SIGUSR1, &sa, NULL); //registrando novo act para lidar com sinal SIGUSR1
