ccd04740550b86715437150709a4abffac764d6c
iuryr <iuryroberto@gmail.com>
2023-10-23 19:00:51 -0300

    Mon 23 Oct 2023 07:00:51 PM -03

--- exploration/decode_word.c
+++ exploration/decode_word.c
@@ -6,7 +6,7 @@ int main(void)
 	static unsigned char letra_decoded;
 	unsigned char rightmost_bit;
 
-	letra = 0b01100001;
+	letra = 'z';
 
 	int i = 0;
 	while (i < 8)
@@ -14,12 +14,11 @@ int main(void)
 		rightmost_bit = letra & 0x80;
 		rightmost_bit >>= 7;
 		letra <<= 1;
-		letra_decoded += rightmost_bit;
+		letra_decoded = letra_decoded | rightmost_bit; //isso aqui precisa mudar
 		if (i == 7)
 			break;
 		letra_decoded <<= 1;
 		i++;
 	}
 	printf("%c\n", letra_decoded);
-
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: iusantos <iusantos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 16:07:48 by iusantos          #+#    #+#             */
-/*   Updated: 2023/10/23 17:31:27 by iusantos         ###   ########.fr       */
+/*   Updated: 2023/10/23 18:49:37 by iusantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,49 @@
 
 void	sig_handler(int sig, siginfo_t *info, void *context)
 {
+	static int i;
+
 	if (sig == SIGUSR1)
 	{
+			i++; // mudar de linha
+			//accumulate bit
 			ft_printf("SIGUSR1 Received from %d\n", info->si_pid);
+			ft_printf("Signals received until now: %i\n", i);
+			// kill(info->si_pid, SIGUSR1);
 	}
 	else if (sig == SIGUSR2)
 	{
+			i++; // mudar de linha
+			//accumulate bit
 			ft_printf("SIGUSR2 Received from %d\n", info->si_pid);
+			ft_printf("Signals received until now: %i\n", i);
+			// kill(info->si_pid, SIGUSR1);
+	}
+	if (i == 8)
+	{
+		//colocar bit acumulado na string
+		i = 0; //reiniciar contagem
 	}
 	context = NULL;
+}
+
+void	accumulate_bit(unsigned char *c, unsigned char bit)
+{
+
 }
 
 int main(void)
 {
 	pid_t	server_pid;
 	struct	sigaction sa;
+	static	unsigned char c;
 
 	sa.sa_sigaction = sig_handler;
 	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
+
 	server_pid = getpid();
 	ft_printf("Server PID: %d\n", server_pid);
 	ft_printf("Waiting for message...\n");
